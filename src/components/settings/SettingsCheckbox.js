7c71acc65f9f03ebc9b9d29d6987aa9db49fa2a3
Adeeb Mohammed <adeebm@gmail.com>
2021-04-23 16:23:45 +0900

    Rewrite SettingsCheckbox and remove dependencies

--- src/components/settings/SettingsCheckbox.js
+++ src/components/settings/SettingsCheckbox.js
@@ -1,32 +1,42 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { CheckBox } from 'react-native-elements';
+import { StyleSheet, View, Text, Pressable, Platform, PlatformColor } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 
-const SettingsCheckbox = ({ title, subtitle, checked, fontFamily, onPress, disabled }) => (
-  <CheckBox
-    checked={!disabled && checked}
-    iconRight
-    uncheckedIcon=""
-    checkedIcon="check"
-    iconType="entypo"
-    containerStyle={StyleSheet.flatten([styles.container, disabled && styles.disabled])}
+const SettingsCheckbox = ({ title, subtitle, checked, fontFamily, onPress }) => (
+  <Pressable
     onPress={onPress}
-    disabled={disabled}
-    title={
-      <View style={styles.textContainer}>
-        <Text style={styles.titleText}>{title}</Text>
-        <Text style={{ ...styles.kanaText, fontFamily }}>{subtitle}</Text>
-      </View>
-    }
-    wrapperStyle={styles.wrapper}
-  />
+    style={({ pressed }) => [
+      {
+        backgroundColor: pressed
+          ? Platform.select({
+              ios: PlatformColor('quaternaryLabel'),
+              default: '#d1d1d6',
+            })
+          : Platform.select({
+              ios: PlatformColor('systemBackground'),
+              default: 'white',
+            }),
+      },
+      styles.checkbox,
+    ]}
+  >
+    <View style={styles.textContainer}>
+      <Text style={styles.titleText}>{title}</Text>
+      <Text style={{ ...styles.kanaText, fontFamily }}>{subtitle}</Text>
+    </View>
+    <Entypo name="check" size={24} color="#2089dc" style={{ opacity: checked ? 1 : 0 }} />
+  </Pressable>
 );
 
 export default SettingsCheckbox;
 
 const styles = StyleSheet.create({
-  wrapper: {
-    justifyContent: 'space-between',
+  checkbox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 16,
+    paddingRight: 16,
+    minHeight: 50,
   },
   titleText: {
     fontSize: 16,
@@ -44,14 +54,5 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     marginRight: 10,
-  },
-  container: {
-    backgroundColor: 'white',
-    margin: 0,
-    marginLeft: 0,
-    marginRight: 0,
-    paddingRight: 16,
-    paddingLeft: 16,
-    borderWidth: 0,
   },
 });
